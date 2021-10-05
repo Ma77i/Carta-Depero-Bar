bc870a17d9ad4ac2a6731be31656f0f85fde9099
Ma77i <mattiasuru@gmail.com>
2021-10-05 14:55:47 -0300

    agregue sticky al header

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+class Producto {
+    constructor ({id, name, platos}) {
+        this.id = id,
+        this.name = name,
+        this.platos = platos
+    }
+
+    renderElemento() {
+    let contenedor = document.createElement('div');
+    
+    contenedor.innerHTML =  `<h2>${this.name}</h2>
+                            <div class='cajas'>
+                                <p class='one'>Nombre</p>
+                                <p class='two'>Precio</p>
+                                <p class='three'>Descripcion</p>
+                            </div>`
+    return contenedor
+    }
+}
+
+
 fetch('/data.json')
     .then(response => {
         return response.json();
@@ -6,31 +27,21 @@ fetch('/data.json')
         // Work with JSON data here
         console.log(data);
         for (const item of data) {
-            datos.push(new Object(item))
-            seccionComida.appendChild(item.renderElemento())
+            datos.push(new Producto(item))
         }
     })
     .catch(err => {
-    // Do something for an error here
+        // Do something for an error here
     });
-
-const seccionComida = document.getElementById('comida');
-const seccionBebida = document.getElementById('bebida');
-const datos = []
-console.log(datos)
-
-renderElemento = () => {
-    let contenedor = document.createElement('div');
     
-    contenedor.innerHTML =  `<h2></h2>
-                            <div class='cajas'>
-                                <p class='one'>${data.name}</p>
-                                <p class='two'>Precio</p>
-                                <p class='three'>Descripcion</p>
-                            </div>`
-    return contenedor
-}
-
+    const seccionComida = document.getElementById('comida');
+    const seccionBebida = document.getElementById('bebida');
+    const datos = []
+    console.log(datos)
+    
+    
+    
+    
 datos.forEach(element => {
-    
+    seccionComida.appendChild(element.renderElemento())
 });
